675a6d6fd1ba4ee1613a955e05d3e23fe571b634
michalk1924 <michalk1924@gmail.com>
2024-10-30 10:42:04 +0200

    chnage colorsSlector

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import styles from './Card.module.css'
 import cardsService from '../../servies/cards'
-import Colors from '../Colors/Colors'
+import Colors from '../Colors/ColorsSlector'
 import { AiOutlineBgColors } from 'react-icons/ai';
 import { MdDeleteSweep } from 'react-icons/md';
 
@@ -26,8 +26,12 @@ function Card({ card, setCards }) {
 
   const changeText = async (updatedText) => {
     try {
+      if (updatedText === '') {
+        updatedText = "..."
+      }
       await cardsService.updateCard(card.id, { text: updatedText });
       setCards(prev => prev.map(c => c.id === card.id ? { ...c, text: updatedText } : c));
+      setIsInputText(false);
     }
     catch (error) {
       console.log('Error updating card text');
@@ -38,14 +42,22 @@ function Card({ card, setCards }) {
     setCards(prev => prev.map(c => c.id === card.id ? { ...c, text: newText } : c));
   }
 
-  const handleSave = (value) => {
-    const trimmedValue = value.trim();
-    if (trimmedValue && trimmedValue !== card.text) {
-      changeText(trimmedValue);
+  const onColorChange = async (cardId, color) => {
+    try {
+      console.log(cardId, color);
+      await cardsService.updateCard(cardId, { color: color });
+      setCards((prevCards) => {
+        const cardIndex = prevCards.findIndex((c) => c.id === cardId);
+        if (cardIndex === -1) return prevCards;
+        const updatedCards = [...prevCards];
+        updatedCards[cardIndex] = { ...updatedCards[cardIndex], color };
+        return updatedCards;
+      });
     }
-    setIsInputText(false);
-  };
-
+    catch (err) {
+      console.log(err.message);
+    }
+  }
 
   return (
     <div className={`${styles.card} ${styles[card.color]}`}>
@@ -53,12 +65,12 @@ function Card({ card, setCards }) {
         {!isInputText && <span>{card.text}</span>}
         {isInputText && <input type="text" value={card.text}
           onChange={(e) => handleTextChange(e.target.value)}
-          onKeyDown={e => e.key === 'Enter' ? handleSave(e.target.value): {}}
-          onBlur={(e) => handleSave(e.target.value)}/>}
+          onKeyDown={e => e.key === 'Enter' ? changeText(e.target.value) : {}}
+          onBlur={(e) => changeText(e.target.value)} />}
       </h2>
       <button onClick={showColorsF}><AiOutlineBgColors /></button>
       <button onClick={deleteCard}><MdDeleteSweep /></button>
-      {showColors && <Colors cardId={card.id} setCards={setCards} />}
+      {showColors && <Colors id={card.id} onColorChange={onColorChange} />}
     </div>
   )
 }
